ec685599df6df27349f7797933518e906c493bd3
BEAST\cmgle <cmgleason79@gmail.com>
2018-07-12 18:24:31 -0400

    7/12/18 18:24

--- Source/PuzzlePlatforms/PuzzlePlatformsGameMode.cpp
+++ Source/PuzzlePlatforms/PuzzlePlatformsGameMode.cpp
@@ -7,7 +7,7 @@
 APuzzlePlatformsGameMode::APuzzlePlatformsGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonBP/Characters/Player/My_Charactor"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
